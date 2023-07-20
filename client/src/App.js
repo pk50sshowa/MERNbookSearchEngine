769d2d0960f8f353b2594fb0ac8cb1eeea5f28dd
Paul Koehler <paul.koehler@gmail.com>
2023-07-20 11:23:05 -0700

    initial changes made to the src directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// GraphQL API endpoint 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers}) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? 'Bearer ${token}' : '',
+    },
+  };
+});
+
+// Instantiate Apollo Client to use the 'authLink' middleware to retrieve the token from localStorage before each request
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
+        <Routes>
+          <Route path='/' element={<SearchBooks />} />
+          <Route path='/saved' element={<SavedBooks />} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
+        </Routes>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
