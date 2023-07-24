536dece669c59d6185c909c19dbea1a5c0fd5408
Paul Koehler <paul.koehler@gmail.com>
2023-07-23 20:52:32 -0700

    changes to server.js

--- server/server.js
+++ server/server.js
@@ -9,9 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
-
   const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -19,8 +16,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   });
   await server.start();
   server.applyMiddleware({ app });
-  console.log('Use GraphQL at http://localhost:${PORT}${server.graphqlPath}');
-
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
 startApolloServer(typeDefs, resolvers);
@@ -33,10 +29,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('*', (res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log('API server running on port ${PORT}!');
-    
+    console.log(`Server running on port ${PORT}!`);
   })
 });
 
@@ -47,8 +46,3 @@ db.once('open', () => {
 //   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 // });
 //
-// app.get('*', (req, res) => {
-//  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
-
-
